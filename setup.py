f07ae37db71c87c403457045a6e6c66db154ffa9
Shubham Sharma <shubham.sharma@phonepe.com>
2017-12-25 22:15:24 +0530

    Fix entrypoint command

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from distutils.core import setup
 setup(
     name = 'pyfox',
     packages = ['pyfox'],
-    version = '0.21',
+    version = '0.22',
     description = 'Shell for foxtrot',
     author = 'Shubham Sharma',
     author_email = 'shubham.sha12@gmail.com',
     url = 'https://github.com/gabber12/pyfox',
-    download_url = 'https://github.com/gabber12/pyfox/archive/0.21.tar.gz',
+    download_url = 'https://github.com/gabber12/pyfox/archive/0.22.tar.gz',
     classifiers = [],
     entry_points = {
-                    'console_scripts': ['foxtrot=pyfox.commands:main'],
+                    'console_scripts': ['foxtrot=pyfox.commands:command'],
     },
     install_requires=[
         'requests',
